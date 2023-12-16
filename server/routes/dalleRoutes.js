7581c46f4cb3ae104b00095ed6ef43e4266cc976
Test <Hammertyme101@gmail.com>
2023-12-16 00:24:11 -0500

    check6

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,26 +1,25 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const configuration = new Configuration({
+
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-const openai = new OpenAIApi(configuration);
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello from DALL-E!' });
 });
 
-router.route('/').post(async (req, res) => {
+router.route('/').post( async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
+    const aiResponse = await openai.images.generate({
       prompt,
       n: 1,
       size: '1024x1024',
